86fe04dae5fe54fc429bb038b1124b6c60dbaea9
Alex Sandro <geniofuturo@gmail.com>
2015-07-09 21:06:38 -0230

    Fix declaration.

--- lbitwise.cpp
+++ lbitwise.cpp
@@ -94,7 +94,7 @@ static struct luaL_reg bitlib[] = {
 };
 
 
-void DLL_EXPORT lua_lbitwiseopen(lua_State *L) {
+DLL_EXPORT void lua_lbitwiseopen(lua_State *L) {
     lua_state = L;
     luaL_openlib(bitlib, (sizeof(bitlib) / sizeof(bitlib[0])));
 }
